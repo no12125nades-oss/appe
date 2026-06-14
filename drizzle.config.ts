09990e8f7d35c7caed5a645ea3575518de74f279
no12125nades-oss <no12125nades@gmail.com>
2026-06-14 04:59:07 +0300

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,11 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
-const connectionString = process.env.MYSQL_URL;
-if (!connectionString) {
-  throw new Error("MYSQL_URL is required to run drizzle commands");
-}
-
 export default defineConfig({
   schema: "./db/schema.ts",
   out: "./db/migrations",
   dialect: "mysql",
   dbCredentials: {
-    url: connectionString,
+    url: process.env.MYSQL_URL || "",
   },
 });
